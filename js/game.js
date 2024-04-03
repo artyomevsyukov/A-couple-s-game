649ac8caeff570c6f5e9af7d3a87b3b66bd1f2c4
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-04-03 15:29:30 +0300

    created card

--- js/game.js
+++ js/game.js
@@ -1,3 +1,6 @@
+const game = document.getElementById("game");
+console.log(game);
+
 let cardCount = 4;
 let cardNumberArray = [];
 
@@ -18,3 +21,37 @@ for (let i = 0; i < cardNumberArray.length; i++) {
 }
 
 console.log(cardNumberArray);
+
+//Создание карт
+for (const cardNumber of cardNumberArray) {
+    let card = document.createElement("div");
+    card.textContent = cardNumber;
+    game.append(card);
+    card.classList.add("card");
+    card.addEventListener("click", () => {
+        card.classList.add("open");
+    });
+}
+
+/* 
+forof
+создание карт
+create element
+textContent
+append
+вывести карты и стилизовать их
+обработчик онклик
+открытие карточки появление изчезнавение цифр 
+переменные для хранение открытых карт
+условия присвоения переменным карт
+35
+условия открыты две карты или нет
+null
+
+Почему у переменной можно удалить класс? 
+при null присваивается карта элемент div
+
+убрать баг при двойном нажатие на карту
+
+условие вывод результата что игра азвершина 
+ */
